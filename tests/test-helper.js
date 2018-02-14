446502eda85066d53ea2ba9d8c244580fecd764c
Dan Freeman <dfreeman@salsify.com>
2018-02-13 22:34:37 -0500

    Refresh dependencies

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,8 @@
+import Application from '../app';
+import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import Application from 'dummy/app';
-import config from 'dummy/config/environment';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
 start();
